b6bb1d76d752a1753e817e75703df0fc971db5a7
Cesar Torres <cearto@berkeley.edu>
2017-08-07 07:40:47 +0000

    adding happy seed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,16 @@
 class LocationsController < ApplicationController
     
     def new_location
+        @location = Location.new()
+        @location.save
     end
     
     def create_location
-        new = Location.create()
     end
     
+    private
+        def choice_params
+        
+            params.require(:id).permit(:latitude, :longitude, :title)
+        end
 end
